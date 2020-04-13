c49bad89933154a61cf3eee76fc93904e2a4b7e0
Natalia <natalia-chekunova@ewave.com>
2020-04-13 18:18:27 +0300

    Paddings fixed

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,10 +1,6 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
-
-import java.util.List;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
@@ -44,9 +40,6 @@ public class GoogleTest {
 
         //to close browser:
         getWebDriver().close();
-
-
-
 
 
     }
